cb4dc4151175026efa32eb44c880a2761c00f0aa
tejaslakade99 <tejaslakade99@gmail.com>
2024-11-03 09:59:12 +0530

    NOTE:Base for Vanila CSS and TailwindCSS

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'
+
+// Component Imports
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.css'
+const Login = () => {
+  return (
+    <div>
+      hellow Login
+    </div>
+  )
+}
+
+export default Login
